511a7303e7ae172260e96c2c74983b3e82d77e02
Mike Wilkes <mikewilkes@outlook.com>
2022-12-18 07:32:58 -0800

    working getPrice function

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,16 @@
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
-  this.price = 10;
+  if (size === "small") {
+    this.price = 10;
+  } else if (size === "medium") {
+    this.price = 14;
+  } else if (size === "large") {
+    this.price = 18;
+  }
 }
 
 Pizza.prototype.getPrice = function() {
-  this.price;
-  if (size === "small") {
-    price = 10;
-  } else if (size === "medium") {
-    price += 14;
-  } else if (size === "large") {
-    price += 18;
-  }
-  return price;
+  return this.price;
   };
+
