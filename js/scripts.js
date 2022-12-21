8bbb9fd94dd53bf40bfbd124a33378ccf0a6b516
Mike Wilkes <mikewilkes@outlook.com>
2022-12-21 07:21:32 -0800

    WIP add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,22 @@ function Pizza(toppings, size) {
   }
 }
 
-Pizza.prototype.getPrice = function() {
+Pizza.prototype.getPrice = function () {
   return this.basePrice + this.toppings.length * 1.5;
-  };
+};
+const toppings = [];
+const size = "";
 
+const pizza = new Pizza(toppings, size);
+const pizzaPrice = pizza.getPrice();
 
+window.addEventListener("load", function () {
+  let form = document.querySelector("form");
+
+  form.addEventListener("submit", function (event) {
+    // i don't think I need this if I'm just displaying price
+    //const sizeInput = document.querySelector('input[name="size"]:checked');
+    document.querySelector("#price").innerText = "$" + price;
+    event.preventDefault();
+  });
+});
